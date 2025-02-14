ab5897dff532b1316801c94f431f4d6a79522b43
davidbotero100 <dboterov@hotmail.com>
2025-02-14 15:47:45 -0500

    updated characters in formValidation.js

--- assets/js/formValidation.js
+++ assets/js/formValidation.js
@@ -4,30 +4,28 @@ function validateForm() {
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    // Check if name contains only alphabets and spaces
-    const namePattern = /^[A-Za-z\s]+$/;
+    // name contains only alphabets and spaces including ñÑ
+    const namePattern = /^[A-Za-zñÑ\s]+$/;
     if (!namePattern.test(name)) {
         alert("Please enter a valid name.");
         return false;
     }
 
-    // Basic email validation pattern
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // email validation
+    const emailPattern = /^[a-zA-Z0-9ñÑ._%+-]+@[a-zA-Z0-9ñÑ.-]+\.[a-zA-Z]{2,}$/;
     if (!emailPattern.test(email)) {
         alert("Please enter a valid email.");
         return false;
     }
 
-    // Ensure message is not too long
     if (message.length > 1000) {
         alert("Your message is too long! Please shorten it.");
         return false;
     }
 
-
     return true;
 }
 
 function sanitizeInput(input) {
-    return input.replace(/[<>]/g, '');  // Basic example, replaces < and > with nothing
+    return input.replace(/[<>]/g, '');
 }
